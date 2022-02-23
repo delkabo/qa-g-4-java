1d231e9db7cf39ab5d65691dc2a5e6fd0a9ba98c
delkabo <Kam1naGit969!>
2022-02-23 20:38:01 +0300

    synchronize with DimaTuchs

---
+++ src/main/java/guru/qa/Book.java
@@ -0,0 +1,17 @@
+package guru.qa;
+
+public class Book {
+        String author = "Камиль";
+        String text = "Мурзилка";
+
+        public void doReadBook() {
+            System.out.println("Книга прочитана " + author);
+            System.out.println("Тест");
+            System.out.println("Test");
+            System.out.println("показывают ");
+            System.out.println("Этот метод выполняется ");
+        }
+
+        //метод от функции отличается тем что нужен для описания действий объекта
+
+}
